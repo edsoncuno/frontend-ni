3d38885088d6a3d41bbd6f61e9a32251ec9f3e60
edsoncuno <edsoncuno1@gmail.com>
2022-02-28 13:15:40 -0500

    estoy usando react router dom y implemente el uso de una layout y su home

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from './App';
+import ReactDOM from 'react-dom'
+
+import { BrowserRouter } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import About from './pages/About'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>, document.getElementById('root')
 );
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,9 +3,9 @@ import React from "react";
 export default class App extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                App
-            </React.Fragment >
+            <div>
+                Not Found
+            </div>
         );
     }
 }
